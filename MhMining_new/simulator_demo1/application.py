8326c7c27a91913efdf4269378d7a2bde9e18c9e
Misa <mmakino@s.h.k.u-tokyo.ac.jp>
2020-06-06 21:02:33 +0900

    6th

--- MhMining_new/simulator_demo1/application.py
+++ MhMining_new/simulator_demo1/application.py
@@ -3,8 +3,11 @@ from io import BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import pandas as pd
+import pandas.plotting as plotting
 import numpy as np
+
 
 app = Flask(__name__)
 
@@ -19,19 +22,13 @@ def Simulation():
 
 @app.route('/simulate1')
 def simulate1():
-# This function read two parameters from web request
-# Returns the sum of two parameters.
-    param1 = request.args.get('input1')
-    param2 = request.args.get('input2')
-    param3 = request.args.get('input3')
-    param4 = request.args.get('input4')
-    param5 = request.args.get('input5')
-    param6 = request.args.get('input6')
-    param7 = request.args.get('input7')
-    param8 = request.args.get('input8')
-    param9 = request.args.get('input9')
+    # This function read two parameters from web request
+    # Returns the sum of two parameters.
+    button1 = request.args.get('input1')
+    button2 = request.args.get('input2')
+    #button3 = request.args.get('input3')
 
-    output1 = int(param1)+ int(param2)
+    output1 = int(button1)+ int(button2)
     output2 = 9 / 8
     # replace the next line with your simulator
 
@@ -49,21 +46,23 @@ def simulate1():
 
 @app.route('/graph1.png')
 def graph1():
-    # 本当はoutput1とoutput2を縦軸と横軸に使いたい
-    # x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     x = np.random.rand(10)*2E+12 + 2E+12
     y = np.random.rand(10) 
-    #y = [5.5E+12, 2E+12, 3E+12, 6E+12,8E+12,2E+12, 2E+12, 2E+12, 2E+12, 2E+12]
+    
+    fig, ax = plt.subplots(2,1,figsize=(5,5))
+    ax[0].scatter(x, y, s=80, c='g')
+    ax[0].set_title('Cost and Risk')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.clear()
+# 色変えたよおおおおおおおおおおお
+    ax[0].scatter(x[3],y[3], s=100, c='r')
+    # plt.scatter(x[3],y[3], s=100, c='r')
 
-    plt.title('Cost and Risk')
-    plt.grid(which='both')
-    plt.legend()
-    plt.scatter(x, y)
-    #ここまでよくわからん
+    df = pd.DataFrame(np.array([['$'+str(20),'$'+str(20),'$'+str(30),'$'+str(20),'$'+str(20),'$'+str(30),'$'+str(20),'$'+str(20),'$'+str(30)],[str(9)+'/TRL',str(4)+'/TRL',str(6)+'/TRL',str(9)+'/TRL',str(4)+'/TRL',str(6)+'/TRL',str(9)+'/TRL',str(4)+'/TRL',str(6)+'/TRL',]]))
+    plotting.table(ax[1], df, loc='center', rowLabels=['Cost','Risk'], colLabels=['Phase1','Phase2','Sum','Phase1','Phase2','Sum','Phase1','Phase2','Sum'])
+    ax[1].axis('off')
+    
+    #plt.show()
 
 # canvasにプロットした画像を出力
     canvas = FigureCanvasAgg(fig)
@@ -74,11 +73,7 @@ def graph1():
     response = make_response(data)
     response.headers['Content-Type'] = 'image/png'
     response.headers['Content-Length'] = len(data)
-    return response
-
-#下には各フェーズのお金の合計金額
-
-#df = pd.read_csv('.csv', encording='utf-8')
+    return response   
 
 if __name__ == "__main__":
     app.run(debug=True)
