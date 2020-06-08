630a26cabd50bb5b3a52835b4ff9efb8d7e838e0
Misa <mmakino@s.h.k.u-tokyo.ac.jp>
2020-06-08 18:55:20 +0900

    8th

---
+++ MhMining_new/si/run.py
@@ -0,0 +1,151 @@
+from flask import Flask, render_template, request, jsonify, json, make_response
+from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import pandas as pd
+import pandas.plotting as plotting
+import numpy as np
+import csv
+import os
+import os.path
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "MhMining Method <br><a href='Simulation'>Simulation</a><br>"
+
+@app.route('/Simulation')
+def Simulation():
+
+    return render_template('mhcalc.html')
+
+@app.route('/simulate1')
+def simulate1():
+    print('run simulate1')
+    global Location_Trans
+    # This function read two parameters from web request
+    # Returns the sum of two parameters.
+    button1 = request.args.get('input1')
+    button2 = request.args.get('input2')
+
+    output1 = int(button1)
+    output2 = int(button2)
+    Location_Trans = [output1, output2]
+    # replace the next line with your simulator
+
+    json_str = '{"output1":' + str(output1) + ',"output2":' + str(output2) + '}'
+    json_data = json.loads(json_str)
+
+    # write your output to a file
+
+    with open('./data/output.json', 'w') as f:
+        json.dump(json_data["output1"], f, indent=4)
+        json.dump(json_data["output2"], f, indent=4)
+    return  json_str
+
+#graphについてかく
+@app.route('/graph1.png')
+def graph1():
+    print('run grraph1')
+    global Location_Trans
+    # 冬眠
+    # button1 = request.args.get('input1')
+    # button2 = request.args.get('input2')
+    print('Location_Trans = ' + str(Location_Trans))
+
+    # sys.exit()
+
+# # csvの選択
+#     FileName_pre = str(button1) + "_" + str(button2) 
+#     for i in range(12):
+#         FileName = FileName_pre + ".csv"
+#         FileDir = str(button1) + "_" + str(button2)
+#         FilePath_soutai = 'scenarios/'+ FileDir + '/' + FileName
+#         base = os.path.dirname(os.path.abspath(__file__))
+#         FilePath = os.path.normpath(os.path.join(base, FilePath_soutai))
+#         #data = pd.read_csv(FilePath,header = None, encoding="shift-jis").values.tolist()
+#         data = pd.read_csv(FilePath_soutai,header = None, encoding="shift-jis").values.tolist()
+
+# # csvの読み込み
+#     with open('./data/inputs.csv', 'r') as f:
+#         reader = csv.reader(f)
+#         inputs = list(reader)
+#         for row in reader:
+#             inputs = [row for row in reader]
+#     x = [i[2] for i in inputs]
+#     y = [float(i[1]) for i in inputs]
+#     data_name = [i[0] for i in inputs]
+
+# サンプルデータ（左からOnshore*Pipeline, Onshore*via Plants, ..., Subsea*Ship）
+    x = [1,2,3,4,5,6,7,8,9]
+    y = [1,2,3,4,5,6,7,8,9]
+    # 9通りのプロット描画
+    fig, ax = plt.subplots(2,1,figsize=(5,5))
+    ax[0].scatter(x, y, s=80, c='g')
+    ax[0].set_title('Cost and Risk')
+
+# ラジオボタンの選択結果の点を強調
+    i = -1 #デフォルト
+    print(Location_Trans)
+    if Location_Trans[0] == 1:
+        if Location_Trans[1] == 4:
+            i = 0
+        elif Location_Trans[1] == 5:
+            i = 1
+        elif Location_Trans[1] == 6:
+            i = 2
+    elif Location_Trans[0] == 2:
+        if Location_Trans[1] == 4:
+            i = 3
+        elif Location_Trans[1] == 5:
+            i = 4
+        elif Location_Trans[1] == 6:
+            i = 5
+    elif Location_Trans[1] == 3:
+        if Location_Trans[1] == 4:
+            i = 6
+        elif Location_Trans[1] == 5:
+            i = 7
+        elif Location_Trans[1] == 6:
+            i = 8
+    else:
+        print('else')
+
+    ax[0].scatter(x[i],y[i], s=100, c='r')
+
+# ラジオボタンの選択結果の表を描画
+    selected = str(Location_Trans[0]) + '_' + str(Location_Trans[1])
+    csvname = 'data/' + selected + '/' + selected + '.csv'
+    with open(csvname) as f:
+        # print(f.read())
+        reader = csv.reader(f)
+        l = [row for row in reader]
+    print(l)
+
+# 概数に
+    for i in range(9):
+        l[0][i] = int(float(l[0][i])/1E+12)
+
+# 表の中身
+    df = pd.DataFrame(np.array([['$ '+str(l[0][0]),'$ '+str(l[0][1]),'$ '+str(l[0][2]),'$ '+str(l[0][3]),'$ '+str(l[0][4]),'$ '+str(l[0][5]),'$ '+str(l[0][6]),'$ '+str(l[0][7]),'$ '+str(l[0][8])],[str(l[1][0])+' TRL',str(l[1][1])+' TRL',str(l[1][2])+' TRL',str(l[1][3])+' TRL',str(l[1][4])+' TRL',str(l[1][5])+' TRL',str(l[1][6])+' TRL',str(l[1][7])+' TRL',str(round(float(l[1][8]),1))+' TRL',]]))
+    plotting.table(ax[1], df, loc='center', rowLabels=['Cost','Risk'], colLabels=['Phase1','Phase2','Phase3','Phase4','Phase5','Phase6','Phase7','Phase8','Sum'])
+    ax[1].axis('off')
+
+# canvasにプロットした画像を出力
+    canvas = FigureCanvasAgg(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    data = png_output.getvalue()
+# HTML側に渡すレスポンスを生成する
+    response = make_response(data)
+    response.headers['Content-Type'] = 'image/png'
+    response.headers['Content-Length'] = len(data)
+    return response   
+
+if __name__ == "__main__":
+    Location_Trans = [0, 0]
+    app.run(debug=True)
